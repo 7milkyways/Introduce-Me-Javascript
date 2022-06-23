e7f534ddc7795f1fcf00852bc77ed877591302b5
Destiny <omoruyidestiny30@gmail.com>
2022-06-23 04:35:51 +0100

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+// assign variables
+const firstName = "Destiny";
+const lastName = "Omoruyi";
+const height = "5'4";
+const country = "Nigeria";
+
+const fullName = firstName +" "+ lastName;
+
+const introduction = "my name is "+ fullName +" i am "+ height +" tall and i am from "+ country;
+
+// display name
+
+alert(introduction);
